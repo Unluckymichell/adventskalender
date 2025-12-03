1b7f21b5633ae090e4eb0b03a1d0a94b4a749931
code.mischael.dev <code@schlegel.dev>
2025-12-03 16:45:45 +0100

    Etz aber

--- components/Coupon.tsx
+++ components/Coupon.tsx
@@ -3,8 +3,8 @@ import React from 'react'
 
 export default function Coupon({content}: {content: string}) {
   return (
-    <div>
-      <h1 className='font-serif text-3xl'>Gutschein</h1>
+    <div className='bg-amber-100 px-4 py-6 rounded-2xl'>
+      <h1 className='font-serif font-bold mb-2 text-3xl'>Gutschein</h1>
       <p className='font-serif text-md'>{content}</p>
     </div>
   )
